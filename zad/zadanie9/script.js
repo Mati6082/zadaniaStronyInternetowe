e26c85534346191dce0e25b1f9f404e31790c335
Mati6082 <matpakla@gmail.com>
2022-11-17 10:12:44 +0100

    file path correction

--- zad/zadanie9/script.js
+++
@@ -1,7 +0,0 @@
-let cale = document.querySelector(`#cale`)
-let btn = document.querySelector(`button`)
-let wynik = document.querySelector(`#wynik`)
-
-btn.addEventListener(`click`, function () {
-    wynik.innerHTML = `${cale.value} cali to ${Number(cale.value) * 25.3995} milimetrów.`
-})
