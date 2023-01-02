957c2971d6e98c9c8dd38e842afcfb3f0ab6b8c8
Mati6082 <matpakla@gmail.com>
2023-01-02 10:24:10 +0100

    BMI

--- zad/zadanie103_BMI/script.js
+++ zad/zadanie103_BMI/script.js
@@ -7,12 +7,19 @@ function bmi(waga, wzrost) {
 
 function poziom(bmi) {
     switch (true) {
-        case(bmi < 16):
+        case(bmi < 16): return `wygłodzenie`
+        case(bmi >= 16 && bmi < 17): return `wychudzenie`
+        case(bmi >= 17 && bmi < 18.5): return `niedowagę`
+        case(bmi >= 18.5 && bmi < 25): return `optimum`
+        case(bmi >= 25 && bmi < 30): return `nadwagę`
+        case(bmi >= 30 && bmi < 35): return `otyłość I stopnia`
+        case(bmi >= 35 && bmi < 40): return `otyłość II stopnia`
+        case(bmi >= 40): return `otyłość III stopnia`
     }
 }
 
 btn.addEventListener(`click`, () => {
     let waga = parseFloat(document.querySelector(`#waga`).value)
     let wzrost = parseFloat(document.querySelector(`#wzrost`).value)
-    wynik.innerHTML = `${bmi(waga, wzrost)}`
+    wynik.innerHTML = `Twoje BMI wynosi ${Math.round(bmi(waga, wzrost) * 100) / 100}.<br>Oznacza to że masz ${poziom(bmi(waga, wzrost))}.`
 })
