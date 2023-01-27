51bf7785d8cef03167cbab9101693b1fd1ff0a23
Mati6082 <matpakla@gmail.com>
2023-01-27 20:03:22 +0100

    czolg - skonczone

--- teoria/T25obiekty/czolg/script.js
+++ teoria/T25obiekty/czolg/script.js
@@ -2,30 +2,49 @@ class Czolg {
     name
     color
     ammunition = 30
+    calibre = 76
 
-    constructor(nazwa, kolor) {
+    constructor(nazwa, color) {
         this.name = nazwa
-        this.color = kolor
+        this.color = color
     }
 
     changeColor = function (kolor) {
+        let stary_kolor = this.color
         this.color = kolor
+        document.write(`Zmieniono kolor czołgu z ${stary_kolor} na ${kolor}.<br>`)
     }
 
     shoot = function () {
         if (this.ammunition !== 0) {
-            console.log("Booom!")
-            this.ammuniton -= 1
+            document.write(`Booom!<br>Zostało ${this.ammunition} amunicji.<br>`);
+            this.ammunition -= 1;
         }
         else {
-            console.log("Brak amunicji! Przeładuj aby wystrzelić.")
+            document.write("Brak amunicji! Przeładuj aby wystrzelić.")
         }
     }
 
     reload = function () {
-        time = 5
-        for (let i = 0; i < time; i++) {
-            console.log("Przeładowywanie. Pozostało " + time + " sekund.")
-        }
+        document.write(`Czołg przeładowany. Można strzelać!!!<br>`)
+        this.ammunition = 30
+    }
+    
+    info = function () {
+        document.write(`${this.name}<br>Kaliber: ${this.calibre}<br>Kolor: ${this.color}<br>Amunicja: ${this.ammunition}`)
     }
 }
+
+let czolg = new Czolg("T34M", "czarny")
+
+czolg.shoot()
+
+czolg.reload()
+
+czolg.changeColor("niebieski")
+
+czolg.shoot()
+czolg.shoot()
+czolg.shoot()
+
+czolg.info()
