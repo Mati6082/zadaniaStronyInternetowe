cef19f56c15b164e2463fa1ec7a2653798e929cc
matpakla <matpakla@gmail.com>
2023-01-27 09:38:02 +0100

    zadanie T25 punkt_odcinek

---
+++ teoria/T25obiekty/punkt_odcinek/script.js
@@ -0,0 +1,47 @@
+class Punkt {
+    nazwa
+    x
+    y
+
+    constructor(nazwa, wspolrzedna_x, wspolrzedna_y) {
+        this.nazwa = nazwa
+        this.x = wspolrzedna_x
+        this.y = wspolrzedna_y
+    }
+
+    info = function () {
+        document.write(`Punkt ${this.nazwa} ma współrzędne (${this.x};${this.y})`)
+    }
+}
+
+class Odcinek {
+    nazwa
+    punkt1 = new Punkt()
+    punkt2 = new Punkt()
+    
+    constructor(nazwa, punkt_1, punkt_2) {
+        this.nazwa = nazwa
+        this.punkt1 = punkt_1
+        this.punkt2 = punkt_2
+    }
+    
+    info = function () {
+        document.write(`Odcinek o punktach ${punkt1.nazwa} i ${punkt2.nazwa} ma długość `)
+    }
+    
+    dlugosc = function () {
+        
+    }
+}
+
+let A = new Punkt("A", 6, 4)
+let B = new Punkt("B", 3, 6)
+
+A.info()
+document.write(`<br>`)
+B.info()
+document.write(`<br>`)
+
+let AB = new Odcinek("AB", A, B)
+
+AB.info()
