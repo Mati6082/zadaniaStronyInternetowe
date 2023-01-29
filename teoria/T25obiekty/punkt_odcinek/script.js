8269ba479e843695ea732a4baacb6dd33e2af5c7
Mati6082 <matpakla@gmail.com>
2023-01-29 19:28:15 +0100

    punkt

--- teoria/T25obiekty/punkt_odcinek/script.js
+++ teoria/T25obiekty/punkt_odcinek/script.js
@@ -21,21 +21,24 @@ class Odcinek {
     
     constructor(nazwa, punkt_1, punkt_2) {
         this.nazwa = nazwa
-        this.punkt1 = new Punkt(punkt_1)
-        this.punkt2 = new Punkt(punkt_2)
+        this.punkt1 = punkt_1
+        this.punkt2 = punkt_2
     }
-    
+
     info = function () {
         document.write(`Odcinek o punktach ${this.punkt1.nazwa} i ${this.punkt2.nazwa} ma długość `)
+        this.dlugosc()
     }
-    
+
     dlugosc = function () {
-        
+        let X = Math.abs(this.punkt1.x - this.punkt2.x)
+        let Y = Math.abs(this.punkt1.y - this.punkt2.y)
+        document.write(`${Math.sqrt(X*X + Y*Y)}`)
     }
 }
 
-let A = new Punkt("A", 6, 4)
-let B = new Punkt("B", 3, 6)
+let A = new Punkt("A", 1, 1)
+let B = new Punkt("B", 4, 5)
 
 A.info()
 document.write(`<br>`)
