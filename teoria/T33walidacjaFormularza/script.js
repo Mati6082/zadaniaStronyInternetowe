35e85f6ed89631492632fef2c2ec711f464d1979
matpakla <matpakla@gmail.com>
2023-02-28 13:09:37 +0100

    walidacja formularza

---
+++ teoria/T33walidacjaFormularza/script.js
@@ -0,0 +1,6 @@
+const btn = document.querySelector("button#password")
+const form = document.querySelector("form#formularz")
+
+btn.addEventListener("click", () => {
+    const name = document.querySelector("#imie")
+})
