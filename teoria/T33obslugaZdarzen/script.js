d515a55c95b6ff2e55dd309f67cefee9ed022162
Mati6082 <matpakla@gmail.com>
2023-03-13 20:38:10 +0100

    T33obslugaZdarzen

--- teoria/T33obslugaZdarzen/script.js
+++ teoria/T33obslugaZdarzen/script.js
@@ -1,6 +1,21 @@
 const text = document.querySelector("p")
 const div = document.querySelector("div")
 
-div.addEventListener("mouseenter", () => {
-    text.innerHTML = "data"
-})
+const date = new Date()
+
+div.addEventListener("mouseenter", data)
+
+
+div.addEventListener("mouseleave", dataByla)
+
+function data() {
+    text.innerHTML = date
+}
+
+function dataByla() {
+    text.innerHTML = ""
+    div.removeEventListener("mouseenter", data)
+    div.addEventListener("mouseenter", () => {
+        text.innerHTML = "Data już była ;)"
+    })
+}
