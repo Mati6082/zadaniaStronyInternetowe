5a9df899d08b775d257c9b4bebdce54abc4c9c7f
matpakla <matpakla@gmail.com>
2023-03-17 10:15:30 +0100

    T24

---
+++ teoria/T24FunkcjeWbudowane/script.js
@@ -0,0 +1,29 @@
+let tekst1 = "Ala ma kota"
+
+let tekst2 = " ale psa nie ma"
+
+let tekst3 = tekst1.concat(tekst2)
+
+console.log(tekst1.charAt(4))
+
+console.log(tekst3)
+
+console.log(tekst2.endsWith("psa", 8))
+
+console.log(tekst3.indexOf("ma", 10))
+
+console.log(tekst3.lastIndexOf("ma"))
+
+console.log(tekst2.slice(9, 15))
+
+console.log(tekst3.split(" ", 6))
+
+console.log(tekst1.startsWith("Ala"))
+
+console.log(tekst2.substr(1, 3))
+
+console.log(tekst2.substring(5, 12))
+
+console.log(tekst1.toLowerCase())
+
+console.log(tekst2.toUpperCase())
