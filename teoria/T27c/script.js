6236caf9ab69bb7dcec78679589f6fe0dfd8007a
Mati6082 <matpakla@gmail.com>
2023-04-01 00:07:19 +0200

    t27c

--- teoria/T27c/script.js
+++ teoria/T27c/script.js
@@ -6,16 +6,16 @@ tablica.style.aspectRatio = "1"
 tablica90.style.aspectRatio = "1"
 
 
-let tab = Array(4)
-let tab90 = Array(4)
+const tab = Array(4)
 
 for (let i = 0; i < 4; i++) {
     tab[i] = Array(4)
-    tab90[i] = Array(4)
 }
+
 
 for (let i = 0; i < 4; i++) {
     const tr = document.createElement("tr")
+    tr.id = i.toString()
     for (let j = 0; j < 4; j++) {
         tab[i][j] = Math.floor(Math.random() * 2)
         const td = document.createElement("td")
@@ -26,16 +26,53 @@ for (let i = 0; i < 4; i++) {
 }
 
 main.append(tablica)
+const p1 = document.createElement("p")
+p1.innerHTML = `Wiersze tablicy, w których suma liczb jest największa:`
+main.append(p1)
+
+let suma = Array(4).fill(0)
 
 for (let i = 0; i < 4; i++) {
-    const tr = document.createElement("tr")
-    for (let j = 3; j >= 0; j++) {
-        tab90[j][i] = tab[i][j]
-        const td = document.createElement("td")
-        td.innerHTML = tab[j][i]
-        tr.append(td)
+    for (let j = 0; j < 4; j++) {
+        if (tab[i][j] === 1) {
+            suma[i] += 1
+        }
     }
-    tablica90.append(tr)
+}
+
+let sumaMax = suma[0]
+let sumaMaxRow = 0
+
+for (let i = 0; i < 4; i++) {
+    if (suma[i] > sumaMax) {
+        sumaMax = suma[i]
+        sumaMaxRow = i
+    }
+}
+
+const tablicaMaxSuma = document.createElement("table")
+
+for (let i = 0; i < 4; i++) {
+    if (suma[i] === sumaMax) {
+        const tr = tablica.children[i]
+        const tr2 = tr.cloneNode(true)
+        tablicaMaxSuma.append(tr2)
+    }
+}
+
+main.append(tablicaMaxSuma)
+const p2 = document.createElement("p")
+p2.innerHTML = "Tablica odwrócona o 90°:"
+main.append(p2)
+
+for (let i = 0; i < 4; i++) {
+    const tr90 = document.createElement("tr")
+    for (let j = 3; j >= 0; j--) {
+        const td90 = document.createElement("td")
+        td90.innerHTML = tab[j][i]
+        tr90.append(td90)
+    }
+    tablica90.append(tr90)
 }
 
 main.append(tablica90)
